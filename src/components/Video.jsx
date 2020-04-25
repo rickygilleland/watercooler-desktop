36e3b12f7d3e881de978c31cc668ea744095aa43
Ricky Gilleland <ricky@strikr.co>
2020-04-25 13:14:07 -0400

    Show microphone on bottom right of videos

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Col, Row } from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faVideoSlash } from '@fortawesome/free-solid-svg-icons';
+import { faVideoSlash, faMicrophone, faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons';
 
 function Video(props) {
     const { videoSizes, publisher, renderVideo } = props;
@@ -14,7 +14,21 @@ function Video(props) {
                     <div className="mx-auto position-relative text-light"  style={{height: videoSizes.height, width: videoSizes.width }}>
                         <video autoPlay ref={renderVideo(publisher.stream)} className="rounded shadow" style={{height: videoSizes.height, width: videoSizes.width }}></video>
                         <div className="position-absolute overlay" style={{bottom:0,backgroundColor:"rgb(18, 20, 34, .5)",width:"100%"}}>
-                            <p className="pl-2 mb-1 mt-1 font-weight-bolder">{publisher.member.name}</p>
+                            <Row>
+                                <Col>
+                                    <p className="pl-2 mb-1 mt-1 font-weight-bolder">{publisher.member.name}</p>
+                                </Col>
+                                <Col>
+                                    <p className="pr-2 mb-1 mt-1 font-weight-bolder text-right">
+                                        {publisher.hasAudio 
+                                            ? 
+                                                <FontAwesomeIcon style={{color:"#2eb97b"}} icon={faMicrophone} /> 
+                                            : 
+                                                <FontAwesomeIcon style={{color:"#f9426c"}} icon={faMicrophoneSlash} /> 
+                                        }
+                                    </p>
+                                </Col>
+                            </Row>
                         </div>
                     </div>
                 </div>
