51193ec381821c84a10fbb037d4b8825115d1222
Ricky Gilleland <ricky@watercooler.work>
2020-05-06 23:57:11 -0400

    Update sentry init

--- src/main.js
+++ src/main.js
@@ -125,7 +125,11 @@ const createWindow = () => {
       frame: false,
       webPreferences: {
         nodeIntegration: true,
-        preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
+        preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
+        preload: init({
+          dsn: 'https://20e5d4f5d6d94630a28e5684a3048940@o281199.ingest.sentry.io/5176374',
+          release: 'watercooler-desktop@' + app.getVersion()
+        })
       }
     });
 
--- src/sentry.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-    init({
-        dsn: 'https://20e5d4f5d6d94630a28e5684a3048940@o281199.ingest.sentry.io/5176374',
-        release: 'watercooler-desktop@' + app.getVersion()
-    });
-}
