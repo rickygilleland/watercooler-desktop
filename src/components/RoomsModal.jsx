0f192870d5ce08e3d77c3dd1e15b5bf57b205ceb
Ricky Gilleland <ricky@watercooler.work>
2020-05-13 23:12:19 -0400

    Disable video rooms until we have plans in place

--- src/components/RoomsModal.jsx
+++ src/components/RoomsModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Row, Col, Button, Navbar, Dropdown, Modal, Card, Image, Form, Alert } from 'react-bootstrap';
+import { Row, Col, Button, Navbar, Dropdown, Modal, Card, Image, Form, Alert, OverlayTrigger, Overlay, Popover, Tooltip } from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faCircleNotch, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 
@@ -75,16 +75,21 @@ function RoomsModal(props) {
                   <p className="text-muted" style={{fontSize:".8rem"}}>We recommend setting the channel to audio only unless you'll be using it for face to face meetings.</p>
                 </Col>
                 <Col className="text-right">
-                  <Form.Check 
-                    type="switch"
-                    id="video_enabled_switch"
-                    name="video_enabled"
-                    checked={videoEnabled}
-                    label=""
-                    size="lg"
-                    onChange={handleVideoEnabledChange}
-                    style={{marginTop:"1.9rem"}}
-                  />
+                  <OverlayTrigger overlay={<Tooltip id="tooltip-view-members">Video rooms are unavailable on the free plan.</Tooltip>}>
+                      <span className="d-inline-block">
+                        <Form.Check 
+                          type="switch"
+                          id="video_enabled_switch"
+                          name="video_enabled"
+                          checked={videoEnabled}
+                          label=""
+                          size="lg"
+                          onChange={handleVideoEnabledChange}
+                          style={{marginTop:"1.9rem", pointerEvents: 'none'}}
+                          disabled
+                        />
+                      </span>
+                  </OverlayTrigger>
                 </Col>
               </Row>
               <Row>
