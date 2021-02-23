c1d8f8f568631ef67a4df1a9b8dd7c79cbe5535a
Ricky Gilleland <ricky.gilleland@gmail.com>
2021-02-23 11:11:55 -0500

    Fix video padding

--- packages/desktop/src/components/Room.tsx
+++ packages/desktop/src/components/Room.tsx
@@ -1046,7 +1046,10 @@ export default function Room(props: RoomProps): JSX.Element {
             !roomAtCapacity &&
             publishersWithMembersData.length > 0 &&
             hasVideoPublishers && (
-              <VideoContainer hasVideoPublishers={hasVideoPublishers}>
+              <VideoContainer
+                hasVideoPublishers={hasVideoPublishers}
+                hasAudioPublishers={hasAudioPublishers}
+              >
                 <VideoList
                   videoSizes={videoSizes}
                   publishers={publishersWithMembersData}
@@ -1144,7 +1147,8 @@ const AudioContainer = styled.div<{
     props.hasVideoPublishers && props.hasAudioPublishers
       ? "2px solid rgb(0, 0, 0, 0.15)"
       : undefined};
-  padding-right: ${(props) => (props.hasVideoPublishers ? "24px" : undefined)};
+  padding-right: ${(props) =>
+    props.hasVideoPublishers && props.hasAudioPublishers ? "24px" : undefined};
   align-content: center;
   align-items: center;
   width: ${(props) => (props.hasVideoPublishers ? undefined : "100%")};
@@ -1152,8 +1156,10 @@ const AudioContainer = styled.div<{
 
 const VideoContainer = styled.div<{
   hasVideoPublishers: boolean;
+  hasAudioPublishers: boolean;
 }>`
   justify-content: center;
   width: 100%;
-  padding: 24px;
+  padding: ${(props) =>
+    props.hasVideoPublishers && props.hasAudioPublishers ? "24px" : undefined};
 `;
