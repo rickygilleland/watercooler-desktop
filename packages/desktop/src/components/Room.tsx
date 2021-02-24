344ded51f64394fbbc2a6f993731ca35bfa5527a
Ricky Gilleland <ricky.gilleland@gmail.com>
2021-02-24 00:20:24 -0500

    Create instance of BlazeFace model and start running recognition

--- packages/desktop/src/components/Room.tsx
+++ packages/desktop/src/components/Room.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {
-  Button,
-  Container,
-  Dropdown,
-  OverlayTrigger,
-  Tooltip,
-} from "react-bootstrap";
+  BlazeFaceModel,
+  NormalizedFace,
+  load,
+} from "@tensorflow-models/blazeface";
+import { Button, Container, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { PropsFromRedux } from "../containers/RoomPage";
 import {
@@ -27,7 +27,7 @@ import {
 } from "../hooks/room";
 import { RouteComponentProps } from "react-router";
 import { Routes } from "./RootComponent";
-import { Thread, Transfer, Worker, spawn } from "threads";
+import { VideoCropping } from "../workers/videoCropping";
 import {
   faArrowLeft,
   faCircleNotch,
@@ -46,10 +46,9 @@ import Pusher, { Channel } from "pusher-js";
 import React, { useCallback, useEffect, useState } from "react";
 import ScreenSharingModal from "./ScreenSharingModal";
 import VideoList from "./VideoList";
-import hark from "hark";
+import hark, { Harker } from "hark";
 import styled from "styled-components";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const bodyPix = require("@tensorflow-models/body-pix");
+import type { RenderCanvas } from "../workers/bodyPix";
 
 interface RoomProps extends PropsFromRedux, RouteComponentProps {
   pusherInstance: Pusher | undefined;
@@ -109,6 +108,27 @@ export default function Room(props: RoomProps): JSX.Element {
 
   //needed so the startPublishingStream(); effect doesn't run constantly
   const [startPublishingCalled, setStartPublishingCalled] = useState(false);
+  const [mounted, setMounted] = useState(true);
+
+  const [blazeModel, setBlazeModel] = useState<BlazeFaceModel | undefined>();
+
+  useEffect(() => {
+    return () => {
+      setMounted(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    const loadNet = async () => {
+      const net = await load({
+        maxFaces: 1,
+      });
+
+      setBlazeModel(net);
+    };
+
+    loadNet();
+  }, []);
 
   const {
     localVideoContainer,
@@ -120,8 +140,13 @@ export default function Room(props: RoomProps): JSX.Element {
 
   const { setHeartbeatInterval } = useCreateHeartbeatIntervals();
 
+  const [speechEventsListener, setSpeechEventsListener] = useState<
+    Harker | undefined
+  >();
   const [speakingPublishers, setSpeakingPublishers] = useState<string[]>([]);
-  const [localStream, setLocalStream] = useState<MediaStream | undefined>();
+  const [streamToPublish, setStreamToPublish] = useState<
+    MediaStream | undefined
+  >();
 
   const [hasVideoPublishers, setHasVideoPublishers] = useState(false);
   const [hasAudioPublishers, setHasAudioPublishers] = useState(false);
@@ -133,14 +158,37 @@ export default function Room(props: RoomProps): JSX.Element {
     user.id,
   );
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [bodyPixWorker, setBodyPixWorker] = useState<any>();
+  const [videoCroppingWorker, setVideoCroppingWorker] = useState<
+    VideoCropping | undefined
+  >();
+  const [videoCroppingInterval, setVideoCroppingInterval] = useState<
+    NodeJS.Timeout | undefined
+  >();
 
   useEffect(() => {
     if (room && !room.video_enabled && videoStatus) {
       setVideoStatus(false);
     }
   }, [room, videoStatus]);
+
+  /*useEffect(() => {
+    const startVideoCroppingWorker = async () => {
+      const videoCroppingWorker = await spawn<VideoCropping>(
+        new Worker("../workers/videoCropping"),
+      );
+      setVideoCroppingWorker(videoCroppingWorker);
+    };
+
+    startVideoCroppingWorker();
+  }, []);*/
+
+  useEffect(() => {
+    return () => {
+      if (videoCroppingInterval) {
+        clearInterval(videoCroppingInterval);
+      }
+    };
+  }, [videoCroppingInterval]);
 
   /*useEffect(() => {
     const startWorker = async () => {
@@ -177,7 +225,7 @@ export default function Room(props: RoomProps): JSX.Element {
   );
 
   useToggleVideoAudioStatus(
-    localStream,
+    streamToPublish,
     videoStatus,
     audioStatus,
     publishers,
@@ -317,6 +365,16 @@ export default function Room(props: RoomProps): JSX.Element {
     });
   };
 
+  const networkOnline = useOnlineListener();
+  useEffect(() => {
+    if (networkOnline) {
+      reconnectNetworkConnections();
+    } else {
+      disconnectNetworkConnections();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [networkOnline]);
+
   useEffect(() => {
     const startStream = async () => {
       let streamOptions;
@@ -350,16 +408,25 @@ export default function Room(props: RoomProps): JSX.Element {
       setRawLocalStream(rawLocalStream);
     };
 
-    if (!rawLocalStream) {
+    const stopStream = () => {
+      if (!rawLocalStream) return;
+      rawLocalStream.getTracks().forEach((track) => track.stop());
+    };
+
+    if (!rawLocalStream && networkOnline) {
       startStream();
+    }
+
+    if (rawLocalStream && !networkOnline) {
+      stopStream();
     }
 
     return () => {
       if (rawLocalStream) {
-        rawLocalStream.getTracks().forEach((track) => track.stop());
+        stopStream();
       }
     };
-  }, [rawLocalStream, settings.defaultDevices]);
+  }, [rawLocalStream, settings.defaultDevices, networkOnline]);
 
   const handleRemoteStreams = useCallback(() => {
     if (!user) {
@@ -572,125 +639,172 @@ export default function Room(props: RoomProps): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rootMediaHandleInitialized, videoRoomStreamHandle]);
 
+  useEffect(() => {
+    if (
+      !streamToPublish ||
+      speechEventsListener ||
+      videoRoomStreamHandle === undefined
+    )
+      return;
+    const speechEvents = hark(streamToPublish);
+
+    speechEvents.on("speaking", function () {
+      setSpeakingPublishers([
+        ...new Set([...speakingPublishers, user.id.toString()]),
+      ]);
+
+      const dataMsg = {
+        type: "started_speaking",
+        publisher_id: user.id,
+      };
+
+      videoRoomStreamHandle.data({
+        text: JSON.stringify(dataMsg),
+      });
+    });
+
+    speechEvents.on("stopped_speaking", function () {
+      const dataMsg = {
+        type: "stopped_speaking",
+        publisher_id: user.id,
+      };
+
+      videoRoomStreamHandle.data({
+        text: JSON.stringify(dataMsg),
+      });
+
+      const updatedSpeakingPublishers = speakingPublishers.filter(
+        (speakingId) => speakingId !== user.id.toString(),
+      );
+      setSpeakingPublishers(updatedSpeakingPublishers);
+    });
+
+    setSpeechEventsListener(speechEvents);
+  }, [
+    speakingPublishers,
+    streamToPublish,
+    user.id,
+    videoRoomStreamHandle,
+    speechEventsListener,
+  ]);
+
+  useEffect(() => {
+    return () => {
+      if (speechEventsListener) {
+        speechEventsListener.stop();
+      }
+    };
+  }, [speechEventsListener]);
+
   const startPublishingStream = useCallback(async () => {
     setStartPublishingCalled(true);
 
     let publishingStarted = false;
 
-    const publishStream = async () => {
-      if (!user || !rawLocalStream) {
-        return;
-      }
+    if (!user || !streamToPublish) {
+      return;
+    }
 
-      const localStream = localVideoCanvas.captureStream(60);
-
-      rawLocalStream
-        .getAudioTracks()
-        .forEach((track) => localStream.addTrack(track));
-
-      const speechEvents = hark(localStream);
-
-      speechEvents.on("speaking", function () {
-        setSpeakingPublishers([
-          ...new Set([...speakingPublishers, user.id.toString()]),
-        ]);
-
-        const dataMsg = {
-          type: "started_speaking",
-          publisher_id: user.id,
-        };
-
-        videoRoomStreamHandle.data({
-          text: JSON.stringify(dataMsg),
-        });
-      });
-
-      speechEvents.on("stopped_speaking", function () {
-        const dataMsg = {
-          type: "stopped_speaking",
-          publisher_id: user.id,
-        };
-
-        videoRoomStreamHandle.data({
-          text: JSON.stringify(dataMsg),
-        });
-
-        const updatedSpeakingPublishers = speakingPublishers.filter(
-          (speakingId) => speakingId !== user.id.toString(),
-        );
-        setSpeakingPublishers(updatedSpeakingPublishers);
-      });
-
-      //publish our feed
-      videoRoomStreamHandle.createOffer({
-        stream: localStream,
-        media: {
-          audioRecv: false,
-          videoRecv: false,
-          audioSend: true,
-          videoSend: true,
+    //publish our feed
+    videoRoomStreamHandle.createOffer({
+      stream: streamToPublish,
+      media: {
+        audioRecv: false,
+        videoRecv: false,
+        audioSend: true,
+        videoSend: true,
+        data: true,
+      },
+      success: function (jsep: string) {
+        const request = {
+          request: "publish",
+          audio: true,
+          video: true,
           data: true,
-        },
-        success: function (jsep: string) {
-          const request = {
-            request: "publish",
-            audio: true,
-            video: true,
-            data: true,
+        };
+
+        videoRoomStreamHandle.send({ message: request, jsep: jsep });
+
+        const isCurrentPublisher = publishers.find(
+          (publisher) => publisher.id === user.id.toString(),
+        );
+
+        if (!isCurrentPublisher && peerUuid) {
+          setNewPublishers([
+            ...newPublishers,
+            {
+              hasVideo: videoStatus,
+              hasAudio: audioStatus,
+              id: user?.id.toString(),
+              stream: streamToPublish,
+              active: true,
+              display: peerUuid,
+              member: currentWebsocketUser,
+            },
+          ]);
+        }
+
+        publishingStarted = true;
+
+        setPublishing(publishingStarted);
+
+        const heartbeatInterval = setInterval(() => {
+          const dataMsg = {
+            type: "participant_status_update",
+            publisher_id: user.id,
+            video_status: videoStatus,
+            audio_status: audioStatus,
+            face_only_status: false,
           };
 
-          videoRoomStreamHandle.send({ message: request, jsep: jsep });
+          videoRoomStreamHandle.data({
+            text: JSON.stringify(dataMsg),
+          });
+        }, 30000);
 
-          const isCurrentPublisher = publishers.find(
-            (publisher) => publisher.id === user.id.toString(),
-          );
+        setHeartbeatInterval(heartbeatInterval);
+      },
+    });
+  }, [
+    audioStatus,
+    currentWebsocketUser,
+    newPublishers,
+    peerUuid,
+    publishers,
+    setHeartbeatInterval,
+    streamToPublish,
+    user,
+    videoRoomStreamHandle,
+    videoStatus,
+  ]);
 
-          if (!isCurrentPublisher && peerUuid) {
-            setNewPublishers([
-              ...newPublishers,
-              {
-                hasVideo: videoStatus,
-                hasAudio: audioStatus,
-                id: user?.id.toString(),
-                stream: localStream,
-                active: true,
-                display: peerUuid,
-                member: currentWebsocketUser,
-              },
-            ]);
-          }
+  const [startStreamCalled, setStartStreamCalled] = useState(false);
 
-          publishingStarted = true;
+  useEffect(() => {
+    //move all of the stream creation logic here, then only call start publishing
+    //stream once it's ready
+    if (
+      !rawLocalStream ||
+      startStreamCalled ||
+      !localVideoCanvas ||
+      !localVideo ||
+      !blazeModel
+    )
+      return;
 
-          setPublishing(publishingStarted);
-          setLocalStream(localStream);
+    setStartStreamCalled(true);
 
-          const heartbeatInterval = setInterval(() => {
-            const dataMsg = {
-              type: "participant_status_update",
-              publisher_id: user.id,
-              video_status: videoStatus,
-              audio_status: audioStatus,
-              face_only_status: false,
-            };
+    const streamToPublish = localVideoCanvas.captureStream(60);
 
-            videoRoomStreamHandle.data({
-              text: JSON.stringify(dataMsg),
-            });
-          }, 30000);
+    rawLocalStream
+      .getAudioTracks()
+      .forEach((track) => streamToPublish.addTrack(track));
 
-          setHeartbeatInterval(heartbeatInterval);
-        },
-      });
-    };
-
-    if (rawLocalStream) {
-      localVideo.srcObject = rawLocalStream;
-      localVideo.muted = true;
-      localVideo.autoplay = true;
-      localVideo.setAttribute("playsinline", "");
-      localVideo.play();
-    }
+    localVideo.srcObject = rawLocalStream;
+    localVideo.muted = true;
+    localVideo.autoplay = true;
+    localVideo.setAttribute("playsinline", "");
+    localVideo.play();
 
     localVideo.onloadedmetadata = () => {
       if (localVideo) {
@@ -715,59 +829,43 @@ export default function Room(props: RoomProps): JSX.Element {
       localVideoCanvas.height = localVideo.height;
 
       const mainCtx = localVideoCanvas.getContext("2d");
-      const ctxCopy = backgroundBlurVideoCanvasCopy.getContext("2d");
+      let coords: NormalizedFace[];
+
+      const videoCroppingInterval = setInterval(async () => {
+        coords = await blazeModel.estimateFaces(localVideo, false);
+      }, 200);
+
+      setVideoCroppingInterval(videoCroppingInterval);
 
       const getNextFrame = async () => {
-        if (!publishingStarted || !ctxCopy || !mainCtx) {
+        if (!mounted) return;
+        if (!mainCtx) {
           requestAnimationFrame(getNextFrame);
           return;
         }
 
-        /*ctxCopy.drawImage(localVideo, 0, 0);
-        const backgroundBlurFrame = ctxCopy.getImageData(
-          0,
-          0,
-          localVideo.width,
-          localVideo.height,
-        );*/
-
-        //bodyPixWorker(backgroundBlurFrame);
+        if (coords) {
+          console.log("TUCKER SSSS", coords);
+        }
 
         mainCtx.drawImage(localVideo, 0, 0);
-
-        /*if (personSegmentation !== undefined) {
-          bodyPix.drawBokehEffect(
-            localVideoCanvas,
-            localVideo,
-            personSegmentation,
-            15,
-            10,
-            true,
-          );
-        }*/
 
         requestAnimationFrame(getNextFrame);
         return;
       };
 
       getNextFrame();
-      publishStream();
+      setStreamToPublish(streamToPublish);
     };
   }, [
-    audioStatus,
-    backgroundBlurVideoCanvasCopy,
-    currentWebsocketUser,
+    rawLocalStream,
     localVideo,
     localVideoCanvas,
-    newPublishers,
-    peerUuid,
-    publishers,
-    rawLocalStream,
-    setHeartbeatInterval,
-    speakingPublishers,
-    user,
-    videoRoomStreamHandle,
-    videoStatus,
+    backgroundBlurVideoCanvasCopy,
+    videoCroppingWorker,
+    mounted,
+    startStreamCalled,
+    blazeModel,
   ]);
 
   useEffect(() => {
@@ -775,16 +873,6 @@ export default function Room(props: RoomProps): JSX.Element {
       setIsCall(true);
     }
   }, [props.match.path]);
-
-  const networkOnline = useOnlineListener();
-  useEffect(() => {
-    if (networkOnline) {
-      reconnectNetworkConnections();
-    } else {
-      disconnectNetworkConnections();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [networkOnline]);
 
   const reconnectNetworkConnections = useCallback(() => {
     if (!room?.channel_id || !pusherInstance) {
@@ -833,6 +921,7 @@ export default function Room(props: RoomProps): JSX.Element {
       rootMediaHandleInitialized &&
       joinedMediaHandle &&
       rawLocalStream &&
+      streamToPublish &&
       !startPublishingCalled
     ) {
       startPublishingStream();
@@ -843,6 +932,7 @@ export default function Room(props: RoomProps): JSX.Element {
     startPublishingCalled,
     startPublishingStream,
     rawLocalStream,
+    streamToPublish,
   ]);
 
   useEffect(() => {
